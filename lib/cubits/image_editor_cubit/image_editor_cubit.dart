4ee1142ba4d4c187c5e1a098c00bb59f4e8c4326
Bereket <berekettadege09@gmail.com>
2024-02-03 21:53:50 +0300

    change google_ml_kit package to google_mlkit_face_detection

--- lib/cubits/image_editor_cubit/image_editor_cubit.dart
+++ lib/cubits/image_editor_cubit/image_editor_cubit.dart
@@ -3,7 +3,7 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 part 'image_editor_state.dart';
 
